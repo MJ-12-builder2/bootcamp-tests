bb76580b3dc142a90151675064caa1a875d438d2
MJ-12-builder2 <m_ngwekazi@yahoo.com>
2021-08-08 19:56:11 +0200

    countAllFromTown

---
+++ countAllFromTown.js
@@ -0,0 +1,12 @@
+function countAllFromTown(regNum, town){
+ 
+    var list = regNum.split(',');
+    var townList = [];
+    var count = 0;
+    for (var i=0; i<list.length; i++){
+         if(list[i].includes(town)=== true){
+         count++;
+        townList.push(list[i].trim());
+        }
+    }
+}
